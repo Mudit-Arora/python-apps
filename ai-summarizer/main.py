9950263249494012fd97b8d0319148820a31cfe8
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-04-07 15:49:52 -0700

    initial commit

---
+++ ai-summarizer/main.py
@@ -0,0 +1,45 @@
+import requests
+from send_email import send_email
+from langchain.chat_models import init_chat_model
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = "de6c5474f9a34526b968574c151d2bdb"
+url = "https://newsapi.org/v2//top-headlines?" \
+    "category=business&" \
+     "language=en&" \
+    "pageSize=8&" \
+    "sortBy=publishedAt&" \
+    "apiKey=" + api_key
+
+# Make request
+requests = requests.get(url)
+
+# Get a dict with data
+content = requests.json()
+articles = content["articles"]
+#print(articles)
+
+# AI summary 
+model = init_chat_model(model="gemini-3-flash-preview",
+        api_key=os.getenv("GOOGLE_API_KEY"), 
+        model_provider="google-genai",
+        )
+
+prompt = f"""
+You're a news summarizer. 
+Write a short paragraph analyzied those news.
+Add another second paragrph to tell me how they affect the stock market.
+Here are the news articles:
+{articles}
+"""
+
+response = model.invoke(prompt)
+respone_str = response.text
+
+body = "Subject: News Summary\n\n" + respone_str + "\n\n"
+print(body)
+
+body = body.encode("utf-8")
+send_email(message=body) 
---
+++ ai-summarizer/simple_ai.py
@@ -0,0 +1,13 @@
+import os 
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+load_dotenv()
+
+model = init_chat_model(model="gemini-3-flash-preview",
+        api_key=os.getenv("GOOGLE_API_KEY"), 
+        model_provider="google-genai",
+        )
+
+
+repsone = model.invoke("Hello, how are you? Do not include any sign-off or signature in your response.")
+print(repsone.content)
