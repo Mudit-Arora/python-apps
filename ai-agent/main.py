5ce510e3ee9c0162545ce397d49f7682cf4d7ad0
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-04-22 16:37:59 -0700

    basic langchain & gemini usage

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -1,5 +1,25 @@
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+
 load_dotenv()
 
 api_key = os.getenv("GOOGLE_API_KEY")
+
+def get_date():
+    """Get the current date"""
+    return datetime.now().strftime("%Y-%m-%d")
+
+llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", api_key=api_key)
+
+system_prompt = """
+You are a helpful assistant that can answer questions and help with tasks.
+"""
+
+agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
+
+user_query = input("Enter a query: ")
+resposne = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
+print(resposne['messages'][-1].content[0]['text'])
