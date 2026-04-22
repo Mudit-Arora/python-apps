e8e4f043453312b1886d5ba9dbae2b6439163ff6
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-04-22 16:52:20 -0700

    test out with local llm

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
+# from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -13,6 +15,7 @@ def get_date():
     return datetime.now().strftime("%Y-%m-%d")
 
 llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", api_key=api_key)
+# llm = ChatOllama(model="qwen2.5:3b")
 
 system_prompt = """
 You are a helpful assistant that can answer questions and help with tasks.
@@ -23,3 +26,4 @@ agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
 user_query = input("Enter a query: ")
 resposne = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
 print(resposne['messages'][-1].content[0]['text'])
+# print(resposne['messages'][-1].content)
