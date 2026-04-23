32d79f316b512127e9f8530dd6dcfe48ef18fe38
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-04-22 17:22:35 -0700

    implementing continuous convo

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -1,3 +1,4 @@
+import uuid
 import os
 from dotenv import load_dotenv
 from datetime import datetime
@@ -5,6 +6,8 @@ from datetime import datetime
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 # from langchain_ollama import ChatOllama
+from langgraph.checkpoint.sqlite import SqliteSaver # Memory for the agent
+import sqlite3
 
 import gradio as gr
 
@@ -16,6 +19,9 @@ def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
 
+conn = sqlite3.connect("langgraph.db", check_same_thread=False) # can have multiple instances of the app
+checkpoint = SqliteSaver(conn=conn)
+
 llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", api_key=api_key)
 # llm = ChatOllama(model="qwen2.5:3b")
 
@@ -23,19 +29,26 @@ system_prompt = """
 You are a helpful assistant that can answer questions and help with tasks.
 """
 
-agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
+agent = create_agent(model=llm,
+                 tools=[get_date], 
+                 system_prompt=system_prompt, 
+                checkpointer=checkpoint) # Memory for the agent
 
 # user_query = input("Enter a query: ")
 # resposne = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
 # print(resposne['messages'][-1].content[0]['text']) # for Gemini
 # print(resposne['messages'][-1].content) # for local LLM
 
-def chat(message):
-    response = agent.invoke({"messages": [{"role": "user", "content": message}]})
+def chat(message, history, thread_id):
+    config = {"configurable": {"thread_id": thread_id}}
+    response = agent.invoke(
+        {"messages": [{"role": "user", "content": message}]},
+        config=config)
     return response['messages'][-1].content[0]['text']
 
 with gr.Blocks() as demo:
     gr.Markdown("# AI Agent") #H1 heading
-    gr.ChatInterface(fn=chat)
+    thread_id = gr.State(value= lambda: str(uuid.uuid4())) # for generating a unique thread id for each user
+    gr.ChatInterface(fn=chat, additional_inputs=[thread_id])
 
 demo.launch()
