66d850ec79b5e5b338bc010d10c46e7f60993b14
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-04-27 13:40:37 -0700

    testing the package

--- python-package/test.py
+++ python-package/test.py
@@ -1,5 +1,5 @@
-import invoice
+from invoicing import generate_invoice
 
-invoice.generate_invoice("../pdf-invoice-generator/invoices",
+generate_invoice("../pdf-invoice-generator/invoices",
     "output", "product_id", "product_name",
     "amount_purchased", "price_per_unit", "total_price")
