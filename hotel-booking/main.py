806c9de929f5aca3dba1d08722c336a1d1fe022d
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-04-27 11:43:05 -0700

    implement ReservationTicket class

--- hotel-booking/main.py
+++ hotel-booking/main.py
@@ -11,6 +11,7 @@ class User:
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == hotel_id, "name"].squeeze()
 
     def book(self):
         """Book the hotel by changing its availability to no"""
@@ -27,8 +28,19 @@ class Hotel:
             return False
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel_object = hotel_object
+
     def generate(self, customer_name, hotel_object):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking details:
+        Name: {customer_name}
+        Hotel: {hotel_object.name}
+        Hotel ID: {hotel_object.hotel_id}
+        """
+        return content
 
 print(df)
 id = input("Enter the id of the hotel: ")
@@ -37,7 +49,7 @@ if hotel.availability(): # check if that specific hotel is available
     hotel.book()
     name = input("Enter your name: ")
     # instance of the ReservationTicket class
-    reservation_ticket = ReservationTicket(name, hotel)
-    print(reservation_ticket.generate())
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+    print(reservation_ticket.generate(customer_name=name, hotel_object=hotel))
 else:
     print("Hotel not available")
