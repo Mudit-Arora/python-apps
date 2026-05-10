7ab45d258af5efbef109f9f721ad202af43bde8f
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2026-05-10 13:13:21 -0700

    adding secured use of cc feature

--- hotel-booking/main.py
+++ hotel-booking/main.py
@@ -3,7 +3,8 @@ import re
 import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype = {"id": str}) # using id column as str instead of int
-df_cards = pd.read_csv("cards.csv", dtype = {"number": str})
+df_cards = pd.read_csv("cards.csv", dtype = str).to_dict(orient="records") # using all columns as strings and converting to dictionary
+df_secure_cards = pd.read_csv("card_security.csv", dtype = {"number": str}) # using number column as string
 
 class User:
     def view_hotels(self):
@@ -43,6 +44,7 @@ class ReservationTicket:
         """
         return content
 
+# parent class
 class CreditCard:
     def __init__(self, number, expiration, holder, cvc):
         self.number = number
@@ -51,23 +53,36 @@ class CreditCard:
         self.cvc = cvc
 
     def validate(self):
-        card_data = f"{self.number}{self.expiration}{self.holder}{self.cvc}"
+        card_data = {"number": self.number, "expiration": self.expiration, "holder": self.holder, "cvc": self.cvc}
         if card_data in df_cards:
             return True
         else:
             return False
 
+# child class that inherits from the parent class (CreditCard class)
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_secure_cards.loc[df_secure_cards["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
 print(df)
 id = input("Enter the id of the hotel: ")
 hotel = Hotel(id) # create hotel instance with the id
 if hotel.availability(): # check if that specific hotel is available
-    credit_card = CreditCard()
+    credit_card = SecureCreditCard(number="1234567890123456", expiration="12/26", holder="John Doe", cvc="123")
     if credit_card.validate():
-        hotel.book()
-        name = input("Enter your name: ")
-        # instance of the ReservationTicket class
-        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-        print(reservation_ticket.generate(customer_name=name, hotel_object=hotel))
+        if credit_card.authenticate(given_password="mypass1"):
+            hotel.book()
+            name = input("Enter your name: ")
+            # instance of the ReservationTicket class
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate(customer_name=name, hotel_object=hotel))
+        else:
+            print("Credit card not authenticated")
     else:
         print("Credit card not valid")
 else:
